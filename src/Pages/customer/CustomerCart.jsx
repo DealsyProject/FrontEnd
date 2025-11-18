88e3a3846f6d1f07c600acdca7916433b5bdc447
Fathima Majdha <majdhafathima18@gmail.com>
2025-11-18 12:54:52 +0530

    newww

--- src/Pages/customer/CustomerCart.jsx
+++ src/Pages/customer/CustomerCart.jsx
@@ -86,18 +86,7 @@ const decreaseQty = async (cartItemId, currentQty) => {
       console.error("❌ Error clearing cart:", error);
     }
   };
-  const clearCart = async () => {
-    try {
-      const customerId = localStorage.getItem("userId");
-      await axiosInstance.delete(`/Cart/clear`);
-
-      setCart([]); // empty the UI
-      alert("🗑️ Cart cleared!");
-    } catch (error) {
-      console.error("❌ Error clearing cart:", error);
-      alert("Failed to clear cart.");
-    }
-  };
+  
 
   const total = cart.reduce((sum, item) => sum + (item.Price * item.Quantity), 0); // Use Price and Quantity
   const shippingFee = 49;
